d83dbd1b83505e014230f1c82dd875478a5cb4f8
PensivePasta <pensivepasta@gmail.com>
2023-11-07 09:42:35 +0000

    Update to articles table in database

--- routes/articles.js
+++ routes/articles.js
@@ -3,12 +3,27 @@ import db from "../database/db.js";
 
 const router = express.Router();
 
+const handleError = (res, err) => {
+  res.status(500).json({ error: err.message });
+  return;
+};
+
 router.get("/articles", (req, res) => {
   const sql = "SELECT * FROM articles";
   db.all(sql, [], (err, articles) => {
     if (err) {
-      res.status(500).json({ error: err.message });
-      return;
+      handleError(res, err);
+    }
+    res.json(articles);
+  });
+});
+
+router.get("/articles/search/:searchTerm", (req, res) => {
+  const searchTerm = `%${req.params.searchTerm}%`;
+  const sql = "SELECT * FROM articles WHERE title LIKE ?";
+  db.all(sql, [searchTerm], (err, articles) => {
+    if (err) {
+      handleError(res, err);
     }
     res.json(articles);
   });
@@ -19,8 +34,7 @@ router.get("/articles/:articleId", (req, res) => {
   const sql = "SELECT * FROM ARTICLES WHERE article_id = ?";
   db.all(sql, [articleId], (err, article) => {
     if (err) {
-      res.status(500).json({ error: err.message });
-      return;
+      handleError(res, err);
     }
     res.json(article);
   });
@@ -31,11 +45,37 @@ router.get("/articles/author/:authorId", (req, res) => {
   const sql = "SELECT * FROM articles WHERE author_id = ?";
   db.all(sql, [authorId], (err, articles) => {
     if (err) {
-      res.status(500).json({ error: err.message });
-      return;
+      handleError(res, err);
     }
     res.json(articles);
   });
 });
 
+router.post("/articles/:articleId/like", (req, res) => {
+  const articleId = req.params.articleId;
+  const sql = "UPDATE articles SET likes = likes + 1 WHERE article_id = ?";
+  db.run(sql, [articleId], function (err) {
+    if (err) {
+      handleError(res, err);
+      return;
+    }
+    res.json({
+      message: "Like count updated successfully",
+    });
+  });
+});
+
+router.post("/articles/:articleId/dislike", (req, res) => {
+  const articleId = req.params.articleId;
+  const sql = "UPDATE articles SET likes = likes - 1 WHERE article_id = ?";
+  db.run(sql, [articleId], function (err) {
+    if (err) {
+      handleError(res, err);
+      return;
+    }
+    res.json({
+      message: "Like count updated successfully",
+    });
+  });
+});
 export default router;
